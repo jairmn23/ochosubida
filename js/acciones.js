2589b9963eeb6b76c5211588fcb94adb855d73b2
jairmn23 <damcb164e23@gmail.com>
2015-06-08 13:19:05 -0500

    ocho1

--- js/acciones.js
+++ js/acciones.js
@@ -1,42 +1,39 @@
 // JavaScript Document
 $(document).ready(function(e){
-// watchID se refiere a la aceleracion 'actual'
-
+	//watchID de refiere a la aceleracion 'actual'
 var watchID = null;
 document.addEventListener("deviceready", Dispositivo_Listo, false);
 
-// Cuando esta listo el dispositivo
-function Dispositivo_Listo(){
+//Cuando esta listo el dispositivo
+function Dispositivo_Listo() {
 Comienza();
 }
-
 //Empieza la 'observacion' de la aceleracion
 function Comienza() {
-
-// Actualiza la aceleracion cada 2 segundos 
+//Actualiza la aceleracion cada 2 segundos
 //
 var opciones = { frequency: 2000 };
 
-whatchID = navigator.acelerometer.watchAcceleration(Correcto, Error, opciones);
-navigator,geolocation.getCurrentPosition(Localiza, ErrorLocalizacion);
+watchID = navigator.accelerometer.watchAcceleration(Correcto, Error, opciones);
+navigator.geolocation.getCurrentPosition(Localiza, ErrorLocalizacion);
 }
-//Detiene la 'observacion' de la aceleracion
+//Detiene la observacion de la aceleracion
 
-function Detente() {
-if (watchID) {
+function Detente(){
+if(watchID) {
 navigator.accelerometer.clearWatch(watchID);
 watchID = null;
 }
 }
 
-//Correcto: Toma una captura de la aceleracion 
-function Correcto(acceleraration) {
+//Correcto: toma una captura de la aceleracion
+function Correcto(acceleration) {
+
 var element = document.getElementById('acelerometro');
 
-
-element.innerHTML = 'Aceleracion en X: ' + acceleration.x + '<br />' +
-                    'Aceleracion en Y: ' + acceleration.y + '<br />' +
-                    'Aceleracion en Z: ' + acceleration.z + '<br />' +  
+element.innerHTML = 'Aceleracion en X: ' + acceleration.x + '<br/>' + 
+                    'Aceleracion en Y: ' + acceleration.y + '<br/>' + 
+                    'Aceleracion en Z: ' + acceleration.z + '<br/>' + 
                     'Intervalo: '  + acceleration.timestamp + '<br />';
 }
 
@@ -44,34 +41,23 @@ element.innerHTML = 'Aceleracion en X: ' + acceleration.x + '<br />' +
 function Error() {
 alert('Error!');
 }
-//Exito al localizar
+//Exito al localizar 
 function Localiza(posicion) {
 var element = document.getElementById('geolocalizacion');
-element.innerHTML = 'Latitud: ' + posicion.coords.latitude    +'
-<br />' + 
-                    'Longitud: ' + posicion.coords.longitude  +'
-<br />' + 
-                    'Altitud: ' + posicion.coords.altitude  +'
-<br />' +   
-                    'Precision: ' + posicion.coords.accuracy  +   
-'<br />' +
-                    'Precision de Altitud: ' + posicion.coords.altitudeAccuracy
-+ '<br />' +
-                    'Direccion: ' + posicion.coords.heading 
-+ '<br />' +
-                    'Velocidad: ' + posicion.coords.speed
-+ '<br />' +
-                    'Intervalo: ' + posicion.timestamp  +
-'<br />';
+element.innerHTML = 'Latitud: '  + posicion.coords.latitude  + '<br />'  +
+'Longitud: '  + posicion.coords.longitude  +  '<br />'  +
+'Altitud: '  + posicion.coords.altitude  +  '<br />'  +
+'Precision:'  + posicion.coords.accuracy  +  '<br />'  +
+'Precision de Altitud: '  + posicion.coords.altitudeAccuracy + '<br />' + 
+'Direccion: '  + posicion.coords.heading +  '<br />'  +
+'Velocidad: '  + posicion.coords.speed +  '<br/>' +
+'Intervalo: '  + posicion.timestamp  +  '<br />';
 }
 
 //Error en la geolocalizacion
-function ErrorLocalizacion(error){
-alert('codigo: ' +error.code +'\n'+
-'mensaje: ' +error.message+ '\n');
+function ErrorLocalizacion(error) { 
+alert('codigo: '  +error.code  + '\n' +
+      'mensaje: ' +error.message + '\n');
+
 }
 });//documento ready
-
-
-
-
